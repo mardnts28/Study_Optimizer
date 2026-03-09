82bc549a60f8f2ed9524fa6811a895de425e1b82
Justine <mjdeleon02@tip.edu.ph>
2026-03-09 12:45:29 +0800

    StudyOptimizer Landing Page

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "main/index.html")
+
+def register(request):
+    return render(request, 'main/register.html')
+
+def login(request):
+    return render(request, 'main/login.html')
